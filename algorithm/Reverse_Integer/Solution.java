c4966a14885c88bcaf018baec99c44dd988ae62b
ChenZhongPu <chenloveit@gmail.com>
2015-08-24 11:18:36 +0800

    revserse interger c++/java

---
+++ algorithm/Reverse_Integer/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int reverse(int x) {
+        boolean isNegative = false;
+        int magic = 214748364;
+        int y = 0;
+        if(x < 0){
+        	if(x == -2147483648){
+        		return 0;
+        	}
+        	isNegative = true;
+        	x = -x;
+        }
+        while(x != 0){
+        	int n = x % 10;
+        	if(isNegative){
+        		if(-y < -magic){
+        			return 0;
+        		}
+        	}
+        	else{
+        		if(y > magic){
+        			return 0;
+        		}
+        	}
+        	y = y * 10 + n;
+        	x = x / 10;
+        }
+        return isNegative? -y : y;
+    }
+}
